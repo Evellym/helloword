0d8d62bd6b0ed86f3e215d82d013c6976a76c0e7
Evellyn Mayumi <evellynyokoyama15@gmail.com>
2023-04-25 11:09:23 -0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,56 @@
+// EXERCÍCIO 1
+class Produto {
+    constructor(codigo, descricao, preco) {
+    this.codigo = codigo;
+    this.descricao = descricao;
+    this.preco = preco;
+}
+}
+
+    class Cliente {
+        constructor(cpf, nome, endereco) {
+            this.cpf = cpf;
+            this.nome = nome;
+            this.endereco = endereco;
+        }
+    }
+
+class Venda {
+    constructor(id, data, cliente, produto) {
+        this.id = id;
+        this.data = data;
+        this.cliente = cliente;
+        this.produto = produto;
+    }
+}
+
+// EXERCÍCIO 2
+class Prroduto {
+    constructor(codigo, descricao, preco) {
+    this.codigo = codigo;
+    this.descricao = descricao;
+    this.preco = preco;
+}
+tipo() {
+    console.log("a")
+}
+codigo() {
+    console.log("b")
+}
+descricao() {
+    console.log("c")
+}
+preco() {
+    console.log("d")
+}}
+
+class Cliente {
+    constructor(cpf, nome, endereco) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+cpf(){
+    console.log
+}
+}
